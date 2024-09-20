9b7c38d4388fc02eb07b19c89929928c784106b3
seekpan <lixin_qiu@163.com>
2024-09-20 15:35:55 +0800

    测试mall-product模块生成的代码

--- mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
@@ -1,9 +1,12 @@
 package com.msb.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 指定Mapper接口对应的路径
+@MapperScan("com.msb.mall.product.dao")
 public class MallProductApplication {
 
     public static void main(String[] args) {
--- mall-product/src/test/java/com/msb/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/msb/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,42 @@
 package com.msb.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.msb.mall.product.entity.BrandEntity;
+import com.msb.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(classes = MallProductApplication.class)
 class MallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("Apple");
+        brandService.save(brandEntity);
+    }
+
+    @Test
+    void selectAll() {
+        List<BrandEntity> list = brandService.list();
+        for (BrandEntity entity : list) {
+            System.out.println(entity);
+        }
+    }
+
+    @Test
+    void selectById() {
+        List<BrandEntity> list = brandService
+                .list(new QueryWrapper<BrandEntity>().eq("brand_id", 2));
+        for (BrandEntity entity : list) {
+            System.out.println(entity);
+        }
     }
 
 }

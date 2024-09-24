6f1afdaccd6b0416b34fde5e930b94d9d4b7dedd
seekpan <lixin_qiu@163.com>
2024-09-24 15:26:15 +0800

    openfeign远程服务的调用测试用例

--- mall-order/src/main/java/com/msb/mall/order/controller/OrderController.java
+++ mall-order/src/main/java/com/msb/mall/order/controller/OrderController.java
@@ -3,13 +3,13 @@ package com.msb.mall.order.controller;
 import com.msb.common.utils.PageUtils;
 import com.msb.common.utils.R;
 import com.msb.mall.order.entity.OrderEntity;
+import com.msb.mall.order.feign.ProductService;
 import com.msb.mall.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Map;
-
 
 
 /**
@@ -25,12 +25,21 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public R queryProduct() {
+        // OpenFegin 远程调用服务
+        return R.ok().put("products", productService.queryAllBrand());
+    }
+
     /**
      * 列表
      */
     @RequestMapping("/list")
     //@RequiresPermissions("order:order:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = orderService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -42,8 +51,8 @@ public class OrderController {
      */
     @RequestMapping("/info/{id}")
     //@RequiresPermissions("order:order:info")
-    public R info(@PathVariable("id") Long id){
-		OrderEntity order = orderService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        OrderEntity order = orderService.getById(id);
 
         return R.ok().put("order", order);
     }
@@ -53,8 +62,8 @@ public class OrderController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("order:order:save")
-    public R save(@RequestBody OrderEntity order){
-		orderService.save(order);
+    public R save(@RequestBody OrderEntity order) {
+        orderService.save(order);
 
         return R.ok();
     }
@@ -64,8 +73,8 @@ public class OrderController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("order:order:update")
-    public R update(@RequestBody OrderEntity order){
-		orderService.updateById(order);
+    public R update(@RequestBody OrderEntity order) {
+        orderService.updateById(order);
 
         return R.ok();
     }
@@ -75,8 +84,8 @@ public class OrderController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("order:order:delete")
-    public R delete(@RequestBody Long[] ids){
-		orderService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        orderService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
--- mall-product/src/main/java/com/msb/mall/product/controller/BrandController.java
+++ mall-product/src/main/java/com/msb/mall/product/controller/BrandController.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.Map;
 
 
-
 /**
  * 品牌
  *
@@ -26,11 +25,23 @@ public class BrandController {
     private BrandService brandService;
 
     /**
+     * 测试openFeign
+     *
+     * @return
+     */
+    @GetMapping("/all")
+    public R queryAllBrand() {
+        BrandEntity entity = new BrandEntity();
+        entity.setName("华为");
+        return R.ok().put("brands", entity);
+    }
+
+    /**
      * 列表
      */
     @RequestMapping("/list")
 //    @RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -42,8 +53,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
 //    @RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -53,8 +64,8 @@ public class BrandController {
      */
     @RequestMapping("/save")
 //    @RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@RequestBody BrandEntity brand) {
+        brandService.save(brand);
 
         return R.ok();
     }
@@ -64,8 +75,8 @@ public class BrandController {
      */
     @RequestMapping("/update")
 //    @RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -75,8 +86,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
 //    @RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }

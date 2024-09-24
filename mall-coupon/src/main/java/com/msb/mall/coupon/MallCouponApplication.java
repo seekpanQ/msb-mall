025060182bf6e62531c33087caf76e451c50d7cd
seekpan <lixin_qiu@163.com>
2024-09-24 10:11:45 +0800

    服务注册到nacos

--- mall-coupon/src/main/java/com/msb/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/msb/mall/coupon/MallCouponApplication.java
@@ -2,8 +2,11 @@ package com.msb.mall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//放开注册中心
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
     public static void main(String[] args) {
--- mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
@@ -3,10 +3,12 @@ package com.msb.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 // 指定Mapper接口对应的路径
 @MapperScan("com.msb.mall.product.dao")
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {

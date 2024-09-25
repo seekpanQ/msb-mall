245cc2952667710294d945d31fb0fe4f11f6b407
seekpan <lixin_qiu@163.com>
2024-09-25 16:28:06 +0800

    renren-fast注册到nacos;产品类别封装成树形结构

--- mall-product/src/main/java/com/msb/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/msb/mall/product/controller/CategoryController.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-
 
 
 /**
@@ -30,10 +30,16 @@ public class CategoryController {
      */
     @RequestMapping("/list")
 //    @RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = categoryService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    @GetMapping("/listTree")
+    public R listTree(@RequestParam Map<String, Object> params) {
+        List<CategoryEntity> list = categoryService.queryPageWithTree(params);
+        return R.ok().put("data", list);
     }
 
 
@@ -42,8 +48,8 @@ public class CategoryController {
      */
     @RequestMapping("/info/{catId}")
 //    @RequiresPermissions("product:category:info")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
         return R.ok().put("category", category);
     }
@@ -53,8 +59,8 @@ public class CategoryController {
      */
     @RequestMapping("/save")
 //    @RequiresPermissions("product:category:save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -64,8 +70,8 @@ public class CategoryController {
      */
     @RequestMapping("/update")
 //    @RequiresPermissions("product:category:update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -75,8 +81,8 @@ public class CategoryController {
      */
     @RequestMapping("/delete")
 //    @RequiresPermissions("product:category:delete")
-    public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds) {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,18 @@
 package com.msb.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.msb.common.utils.PageUtils;
 import com.msb.common.utils.Query;
-
 import com.msb.mall.product.dao.CategoryDao;
 import com.msb.mall.product.entity.CategoryEntity;
 import com.msb.mall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -24,6 +26,59 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 查询所有的类别数据，然后将数据封装为树形结构，便于前端使用
+     *
+     * @param params
+     * @return
+     */
+    @Override
+    public List<CategoryEntity> queryPageWithTree(Map<String, Object> params) {
+        // 1.查询所有的商品分类信息
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        // 2.将商品分类信息拆解为树形结构【父子关系】
+        // 第一步遍历出所有的大类  parent_cid = 0
+        List<CategoryEntity> list = categoryEntities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                // 根据大类找到多有的小类  递归的方式实现
+                .map(categoryEntity -> {
+                    categoryEntity.setChildrens(getCategoryChildren(categoryEntity, categoryEntities));
+                    return categoryEntity;
+                }).sorted((entity1, entity2) -> {
+                    return (entity1.getSort() == null ? 0 : entity1.getSort())
+                            - (entity2.getSort() == null ? 0 : entity2.getSort());
+                })
+                .collect(Collectors.toList());
+        return list;
+    }
+
+    /**
+     * 查找该大类下的所有的小类  递归查找
+     *
+     * @param categoryEntity   某个大类
+     * @param categoryEntities 所有的类别数据
+     * @return
+     */
+    private List<CategoryEntity> getCategoryChildren(CategoryEntity categoryEntity
+            , List<CategoryEntity> categoryEntities) {
+        List<CategoryEntity> collect =
+                categoryEntities.stream()
+                        // 根据大类找到他的直属的小类
+                        .filter(entity -> {
+                            return categoryEntity.getCatId() == entity.getParentCid();
+                        })
+                        // 根据这个小类递归找到对应的小小类
+                        .map(entity -> {
+                            entity.setChildrens(getCategoryChildren(entity, categoryEntities));
+                            return entity;
+                        }).sorted((entity1, entity2) -> {
+                            return (entity1.getSort() == null ? 0 : entity1.getSort())
+                                    - (entity2.getSort() == null ? 0 : entity2.getSort());
+                        })
+                        .collect(Collectors.toList());
+        return collect;
     }
 
 }

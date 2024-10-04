0fef020020b45670563d0031e818b7501dfd0c1d
seekpan <lixin_qiu@163.com>
2024-10-04 18:20:04 +0800

    品牌logo图片上传

--- mall-third-party/src/test/java/com/msb/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/msb/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -47,7 +47,7 @@ class MallThirdPartyApplicationTests {
         // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
         InputStream inputStream = new FileInputStream("C:\\Users\\seekpan\\Desktop\\work\\personal\\seekpan.jpg");
         // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
-        ossClient.putObject("study-seekpan-mall", "seekpan.jpg", inputStream);
+        ossClient.putObject("study-seekpan-mall", "seekpan222.jpg", inputStream);
 
         // 关闭OSSClient。
         ossClient.shutdown();

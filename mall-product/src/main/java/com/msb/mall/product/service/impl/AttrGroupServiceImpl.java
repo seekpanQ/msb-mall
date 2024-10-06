2963050dabb02ab28031570c9438afd8a0a31435
seekpan <lixin_qiu@163.com>
2024-10-06 23:36:33 +0800

    根据catelogId查询属性分组

--- mall-product/src/main/java/com/msb/mall/product/service/impl/AttrGroupServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,16 +1,17 @@
 package com.msb.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.msb.common.utils.PageUtils;
 import com.msb.common.utils.Query;
-
 import com.msb.mall.product.dao.AttrGroupDao;
 import com.msb.mall.product.entity.AttrGroupEntity;
 import com.msb.mall.product.service.AttrGroupService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -23,6 +24,40 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
                 new QueryWrapper<AttrGroupEntity>()
         );
 
+        return new PageUtils(page);
+    }
+
+    /**
+     * 查询列表数据
+     * 根据列表编号来查询
+     *
+     * @param params
+     * @param catelogId 如何catelogId为0 就不根据catelogId来查询
+     * @return
+     */
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        // 获取检索的关键字
+        String key = (String) params.get("key");
+        QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<>();
+        if (StringUtils.isNotEmpty(key)) {
+            // 拼接查询的条件
+            wrapper.and(obj -> {
+                obj.eq("attr_group_id", key).or().like("attr_group_name", key);
+            });
+        }
+        if (catelogId == 0) {
+            // 不根据catelogId来查询
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params), wrapper
+            );
+            return new PageUtils(page);
+        }
+        // 根据类别编号来查询属性信息
+        wrapper.eq("catelog_id", catelogId);
+        IPage<AttrGroupEntity> page = this.page(
+                new Query<AttrGroupEntity>().getPage(params), wrapper
+        );
         return new PageUtils(page);
     }
 

1fbd9c026b883a099eff196ee9089f49df0aea86
seekpan <lixin_qiu@163.com>
2024-10-08 14:40:21 +0800

    添加分页插件和添加品牌管理的查询条件

--- mall-product/src/main/java/com/msb/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,17 @@
 package com.msb.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.msb.common.utils.PageUtils;
 import com.msb.common.utils.Query;
-
 import com.msb.mall.product.dao.BrandDao;
 import com.msb.mall.product.entity.BrandEntity;
 import com.msb.mall.product.service.BrandService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,9 +19,16 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 1. 获取key
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<>();
+        if (StringUtils.isNotEmpty(key)) {
+            // 添加对应的查询条件
+            wrapper.eq("brand_id", key).or().like("name", key);
+        }
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
 
         return new PageUtils(page);

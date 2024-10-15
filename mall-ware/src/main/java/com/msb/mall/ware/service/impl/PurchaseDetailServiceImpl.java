74b2a259f9c7b9224c2201d7c44ddc0fb36450ab
seekpan <lixin_qiu@163.com>
2024-10-15 15:27:32 +0800

    采购单和采购需求查询管理

--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -8,6 +8,7 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.PurchaseDetailDao;
 import com.msb.mall.ware.entity.PurchaseDetailEntity;
 import com.msb.mall.ware.service.PurchaseDetailService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -18,9 +19,23 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> wrapper = new QueryWrapper<>();
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotEmpty(wareId)) {
+            wrapper.eq("ware_id", wareId);
+        }
+        String status = (String) params.get("status");
+        if (StringUtils.isNotEmpty(status)) {
+            wrapper.eq("status", status);
+        }
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.eq("purchase_id", key).or()
+                    .eq("sku_id", key);
+        }
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseServiceImpl.java
@@ -8,6 +8,7 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.PurchaseDao;
 import com.msb.mall.ware.entity.PurchaseEntity;
 import com.msb.mall.ware.service.PurchaseService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -18,12 +19,25 @@ public class PurchaseServiceImpl extends ServiceImpl<PurchaseDao, PurchaseEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseEntity> wrapper = new QueryWrapper<>();
+        String status = (String) params.get("status");
+        if (StringUtils.isNotEmpty(status)) {
+            wrapper.eq("status", status);
+        }
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.eq("id", key).or()
+                    .eq("assignee_id", key).or()
+                    .eq("ware_id", key).or()
+                    .like("assignee_name", key);
+        }
         IPage<PurchaseEntity> page = this.page(
                 new Query<PurchaseEntity>().getPage(params),
-                new QueryWrapper<PurchaseEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
     }
+
 
 }

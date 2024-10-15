b2116d2e36df1ecbab7b4261294a13caa8488d6d
seekpan <lixin_qiu@163.com>
2024-10-15 11:22:28 +0800

    生成mall-ware按照关键字查询

--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareInfoServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -8,6 +8,7 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.WareInfoDao;
 import com.msb.mall.ware.entity.WareInfoEntity;
 import com.msb.mall.ware.service.WareInfoService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -18,9 +19,17 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.eq("id", key).or()
+                    .like("name", key).or()
+                    .like("address", key).or()
+                    .like("areacode", key);
+        }
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wrapper
         );
 
         return new PageUtils(page);

4b3cf64dce9faf649e0a361365156939c61b04e7
seekpan <lixin_qiu@163.com>
2024-10-21 16:57:57 +0800

    配置elasticsearch客户端

---
+++ mall-search/src/main/java/com/msb/mall/search/config/MallElasticSearchConfiguration.java
@@ -0,0 +1,24 @@
+package com.msb.mall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ElasticSearch的配置类
+ */
+@Configuration
+public class MallElasticSearchConfiguration {
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost("192.168.91.131", 9200, "http"));
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+}
--- mall-search/src/test/java/com/msb/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/msb/mall/search/MallSearchApplicationTests.java
@@ -1,13 +1,19 @@
 package com.msb.mall.search;
 
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallSearchApplicationTests {
 
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
     @Test
     void contextLoads() {
+        System.out.println("----->" + restHighLevelClient);
     }
 
 }

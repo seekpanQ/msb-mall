86c86dc1290186225c48af0245ad9dd28aee60a0
seekpan <lixin_qiu@163.com>
2024-12-04 21:48:49 +0800

    token机制解决提交订单的幂等性

--- mall-order/src/main/java/com/msb/mall/order/service/impl/OrderServiceImpl.java
+++ mall-order/src/main/java/com/msb/mall/order/service/impl/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.msb.mall.order.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.msb.common.constant.OrderConstant;
 import com.msb.common.utils.PageUtils;
 import com.msb.common.utils.Query;
 import com.msb.common.vo.MemberVO;
@@ -12,16 +13,18 @@ import com.msb.mall.order.feign.CartFeginService;
 import com.msb.mall.order.feign.MemberFeginService;
 import com.msb.mall.order.interceptor.AuthInterceptor;
 import com.msb.mall.order.service.OrderService;
-import com.msb.mall.order.vo.MemberAddressVo;
-import com.msb.mall.order.vo.OrderConfirmVo;
-import com.msb.mall.order.vo.OrderItemVo;
+import com.msb.mall.order.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -35,6 +38,8 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
     private CartFeginService cartFeginService;
     @Autowired
     private ThreadPoolExecutor executor;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -74,7 +79,36 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
             e.printStackTrace();
         }
         // 3.计算订单的总金额和需要支付的总金额 VO自动计算
+
+        // 4.生成防重的Token
+        String token = UUID.randomUUID().toString().replaceAll("-", "");
+        // 我们需要把这个Token信息存储在Redis中
+        // order:token:用户编号
+        redisTemplate.opsForValue().set(OrderConstant.ORDER_TOKEN_PREFIX + ":" + memberVO.getId(), token);
+        // 然后我们需要将这个Token绑定在响应的数据对象中
+        vo.setOrderToken(token);
         return vo;
+    }
+
+    @Override
+    public OrderResponseVO submitOrder(OrderSubmitVO vo) {
+        // 需要返回响应的对象
+        OrderResponseVO responseVO = new OrderResponseVO();
+        // 获取当前登录的用户信息
+        MemberVO memberVO = (MemberVO) AuthInterceptor.threadLocal.get();
+        // 1.验证是否重复提交  保证Redis中的token 的查询和删除是一个原子性操作
+        String key = OrderConstant.ORDER_TOKEN_PREFIX + ":" + memberVO.getId();
+        String script = "if redis.call('get',KEYS[1])==ARGV[1] then redis.call('del',KEYS[1]) else return 0 end";
+        Long result = redisTemplate.execute(new DefaultRedisScript<Long>(script, Long.class)
+                , Arrays.asList(key)
+                , vo.getOrderToken());
+        if (result == 0) {
+            // 表示验证失败 说明是重复提交
+            responseVO.setCode(1);
+            return responseVO;
+        }
+
+        return responseVO;
     }
 
 }
--- mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
+++ mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
@@ -1,11 +1,16 @@
 package com.msb.mall.order.web;
 
+import com.msb.common.exception.NoStockExecption;
 import com.msb.mall.order.service.OrderService;
 import com.msb.mall.order.vo.OrderConfirmVo;
+import com.msb.mall.order.vo.OrderResponseVO;
+import com.msb.mall.order.vo.OrderSubmitVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderWebController {
@@ -18,4 +23,34 @@ public class OrderWebController {
         model.addAttribute("confirmVo", confirmVo);
         return "confirm";
     }
+
+    @PostMapping("/orderSubmit")
+    public String orderSubmit(OrderSubmitVO vo, Model model, RedirectAttributes redirectAttributes) {
+        OrderResponseVO orderResponseVO = null;
+        Integer code = 0;
+        try {
+            orderResponseVO = orderService.submitOrder(vo);
+            code = orderResponseVO.getCode();
+        } catch (NoStockExecption e) {
+            code = 2;
+        }
+        if (code == 0) {
+            model.addAttribute("orderResponseVO", orderResponseVO);
+            // 表示下单操作成功
+            return "pay";
+        } else {
+            System.out.println("code=" + code);
+            String msg = "订单失败";
+            if (code == 1) {
+                msg = msg + ":重复提交";
+            } else if (code == 2) {
+                msg = msg + ":锁定库存失败";
+            }
+            //redirectAttributes.addAttribute("msg",msg);
+            redirectAttributes.addFlashAttribute("msg", msg);
+            return "redirect:http://order.msb.com/toTrade";
+        }
+    }
+
+
 }

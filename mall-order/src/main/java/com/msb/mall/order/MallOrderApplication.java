0c1631f535d9999f2b667fa8806c1a85c39b3ecb
seekpan <lixin_qiu@163.com>
2024-12-07 09:46:11 +0800

    处理事务传播失效的解决方案复原代码

--- mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableAspectJAutoProxy(exposeProxy = true)
+//@EnableAspectJAutoProxy(exposeProxy = true)
 @EnableRedisHttpSession
 @SpringBootApplication
 @EnableDiscoveryClient
--- mall-order/src/main/java/com/msb/mall/order/interceptor/AuthInterceptor.java
+++ mall-order/src/main/java/com/msb/mall/order/interceptor/AuthInterceptor.java
@@ -21,16 +21,16 @@ public class AuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         //通过HttpSession获取当前登录的用户信息
-//        HttpSession session = request.getSession();
-//        Object attribute = session.getAttribute(AuthConstant.AUTH_SESSION_REDIS);
-//        if (attribute != null) {
-//            MemberVO memberVO = (MemberVO) attribute;
-//            threadLocal.set(memberVO);
-//            return true;
-//        }
-//        // 如果 attribute == null 说明没有登录，那么我们就需要重定向到登录页面
-//        session.setAttribute(AuthConstant.AUTH_SESSION_MSG, "请先登录");
-//        response.sendRedirect("http://auth.msb.com/login.html");
-        return true;
+        HttpSession session = request.getSession();
+        Object attribute = session.getAttribute(AuthConstant.AUTH_SESSION_REDIS);
+        if (attribute != null) {
+            MemberVO memberVO = (MemberVO) attribute;
+            threadLocal.set(memberVO);
+            return true;
+        }
+        // 如果 attribute == null 说明没有登录，那么我们就需要重定向到登录页面
+        session.setAttribute(AuthConstant.AUTH_SESSION_MSG, "请先登录");
+        response.sendRedirect("http://auth.msb.com/login.html");
+        return false;
     }
 }
